d68e4f599947e5f363f9d4b5426c9178635b8d24
Lukasz <lukasz@dom.local>
2019-09-21 22:33:15 +0200

    change target installation directory to /opt/avr, dont download already downloaded sources

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 
 SRC="src"
 BUILD="build"
-INSTALL="avr-gcc" # tar.bz2 needs a prefix of 'avr-gcc'
+INSTALL="/opt/avr" # tar.bz2 needs a prefix of 'avr-gcc'
 DOWNLOAD="download"
 mkdir ${SRC}
 mkdir ${BUILD}
 mkdir ${INSTALL}
+mkdir ${DOWNLOAD}
 root=$(pwd)
 cores=4
 VERSION_BINUTILS="2.32"
@@ -14,32 +15,38 @@ VERSION_GCC="9.1.0"
 VERSION_LIBC="2.0.0"
 
 # Get sources
-wget -q   "https://dl.bintray.com/osx-cross/avr-patches/avr-binutils-${VERSION_BINUTILS}-size.patch" &
-wget -q   "https://dl.bintray.com/osx-cross/avr-patches/avr-libc-${VERSION_LIBC}-atmega168pb.patch" &
-wget -qO- "https://ftp.gnu.org/gnu/binutils/binutils-${VERSION_BINUTILS}.tar.bz2" | tar xj --directory ${SRC} &
-wget -qO- "https://ftp.gnu.org/gnu/gcc/gcc-${VERSION_GCC}/gcc-${VERSION_GCC}.tar.xz" | tar xJ --directory ${SRC} &
-wget -qO- "https://download.savannah.gnu.org/releases/avr-libc/avr-libc-${VERSION_LIBC}.tar.bz2" | tar xj --directory ${SRC} &
-wait
+cd ${DOWNLOAD}
+[ -f avr-binutils-${VERSION_BINUTILS}-size.patch ] || wget   "https://dl.bintray.com/osx-cross/avr-patches/avr-binutils-${VERSION_BINUTILS}-size.patch" 
+[ -f avr-libc-${VERSION_LIBC}-atmega168pb.patch ] || wget   "https://dl.bintray.com/osx-cross/avr-patches/avr-libc-${VERSION_LIBC}-atmega168pb.patch" 
+[ -f binutils-${VERSION_BINUTILS}.tar.bz2 ] || wget  "https://ftp.gnu.org/gnu/binutils/binutils-${VERSION_BINUTILS}.tar.bz2" 
+[ -f gcc-${VERSION_GCC}.tar.xz ] || wget "https://ftp.gnu.org/gnu/gcc/gcc-${VERSION_GCC}/gcc-${VERSION_GCC}.tar.xz" 
+[ -f avr-libc-${VERSION_LIBC}.tar.bz2 ] || wget "https://download.savannah.gnu.org/releases/avr-libc/avr-libc-${VERSION_LIBC}.tar.bz2" 
+
+cd ${root}/${SRC}
+tar xf "${root}/${DOWNLOAD}/binutils-${VERSION_BINUTILS}.tar.bz2"
+tar xf "${root}/${DOWNLOAD}/gcc-${VERSION_GCC}.tar.xz"
+tar xf "${root}/${DOWNLOAD}/avr-libc-${VERSION_LIBC}.tar.bz2"
 
 # Build binutils first
+cd ${root}
 cd ${SRC}/binutils-${VERSION_BINUTILS}
 # patch size file
-patch -g 0 -f -p0 -i ../../avr-binutils-${VERSION_BINUTILS}-size.patch
+patch -g 0 -f -p0 -i ${root}/${DOWNLOAD}/avr-binutils-${VERSION_BINUTILS}-size.patch
 mkdir build && cd build
 # configure and make
-../configure --prefix=${root}/${INSTALL}/avr-binutils/ --target=avr --disable-nls --disable-werror
+../configure --prefix=${INSTALL}/avr-binutils/ --target=avr --disable-nls --disable-werror
 make -j${cores}
 make install
 
 # prepend path of newly compiled avr-gcc
-export PATH=${root}/${INSTALL}/avr-binutils/bin:$PATH
+export PATH=${INSTALL}/avr-binutils/bin:$PATH
 
 cd ${root}
 cd ${SRC}/gcc-${VERSION_GCC}
 mkdir build && cd build
-../configure --target=avr --prefix=${root}/${INSTALL}/avr-gcc/ \
-        --with-ld=${root}/${INSTALL}/avr-binutils/bin/avr-ld \
-        --with-as=${root}/${INSTALL}/avr-binutils/bin/avr-as \
+../configure --target=avr --prefix=${INSTALL}/avr-gcc/ \
+        --with-ld=${INSTALL}/avr-binutils/bin/avr-ld \
+        --with-as=${INSTALL}/avr-binutils/bin/avr-as \
         --enable-languages=c,c++ --with-dwarf2 \
         --disable-nls --disable-libssp --disable-shared \
         --disable-threads --disable-libgomp --disable-bootstrap
@@ -47,16 +54,17 @@ make -j${cores}
 make install
 
 # prepend path of newly compiled avr-gcc
-export PATH=${root}/${INSTALL}/avr-gcc/bin:$PATH
+export PATH=${INSTALL}/avr-gcc/bin:$PATH
 
 cd ${root}
 cd ${SRC}/avr-libc-${VERSION_LIBC}
-patch -g 0 -f -p0 -i ../../avr-libc-${VERSION_LIBC}-atmega168pb.patch
+patch -g 0 -f -p1 -i ${root}/${DOWNLOAD}/avr-libc-${VERSION_LIBC}-atmega168pb.patch
 build=`./config.guess`
-./configure --build=${build} --prefix=${root}/${INSTALL}/avr-gcc --host=avr
+./configure --build=${build} --prefix=${INSTALL}/avr-gcc --host=avr
 make install -j${cores}
 
 cd ${root}
-rm -r build src
-rm avr-binutils-${VERSION_BINUTILS}-size.patch
-rm avr-libc-${VERSION_LIBC}-atmega168pb.patch
+#rm -r build src
+#rm avr-binutils-${VERSION_BINUTILS}-size.patch
+#rm avr-libc-${VERSION_LIBC}-atmega168pb.patch
+
